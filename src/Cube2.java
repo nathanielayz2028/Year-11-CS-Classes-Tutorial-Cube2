b40e908d6a72190544409bb77fddbdb414ca9abf
nathanielayz2028 <nathanielayz2028@student.cis.edu.hk>
2025-11-26 08:55:20 +0800

    finished

--- src/Cube2.java
+++ src/Cube2.java
@@ -12,18 +12,51 @@ public class Cube2 {
         basicCube.setSide(side);
     }
 
+    public Cube2(int side, String color) {
+        this();
+        this.color = color;
+        basicCube.setSide(side);
+    }
+
     public int getSide() {
         return basicCube.getSide();
     }
 
+    public void setSide(int side) {
+        basicCube.setSide(side);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
+    public int calculateVolume() { return basicCube.calculateVolume();}
+    public int calculateSurfaceArea() { return basicCube.calculateSurfaceArea();}
+    public Cube2 add(Cube2 otherCube) {
+        if (basicCube.getSide()*basicCube.getSide() + otherCube.getSide()*otherCube.getSide() != Math.pow((int)Math.sqrt(basicCube.getSide()*basicCube.getSide() + otherCube.getSide()*otherCube.getSide()), 2)) {
+            throw new IllegalArgumentException("Cube lengths are not Pythagorean Triples.");
+        }
+        return new Cube2((int) Math.sqrt(basicCube.getSide()*basicCube.getSide() + otherCube.getSide()*otherCube.getSide()),this.color);
+    }
+    public Cube2 minus(Cube2 otherCube) {
+        if (basicCube.getSide()*basicCube.getSide() - otherCube.getSide()*otherCube.getSide() != Math.pow((int)Math.sqrt(basicCube.getSide()*basicCube.getSide() - otherCube.getSide()*otherCube.getSide()), 2)) {
+            throw new IllegalArgumentException("Cube lengths are not Pythagorean Triples.");
+        }
+        return new Cube2((int) Math.sqrt(basicCube.getSide()*basicCube.getSide() - otherCube.getSide()*otherCube.getSide()),this.color);
+    }
+    public boolean equals(Cube2 otherCube) {
+        if (basicCube.getSide() == otherCube.getSide() && this.color == otherCube.color) {
+            return true;
+        }
+        return false;
+    }
 
     public String toString() {
-        return basicCube.toString();
+        return "Cube{side=" + basicCube.getSide() + ", color=\"" + this.color + "\"}";
     }
 }
